eb588f7bbfcf9cc0355fed5865bc37116cf13e94
Eric Gao <754095810@qq.com>
2020-05-23 02:44:33 +0800

    Global exception handling

--- src/util/axios.js
+++ src/util/axios.js
@@ -33,15 +33,48 @@ axios.interceptors.response.use(
   },
   // 全局异常处理
   error => {
-    let status = error.response.status;
-    switch (status) {
-      case 400:
-        Message.error("该学生已完成互选");
-        break;
-      case 405:
-        Message.error("您的互选学生数量已达上限");
-        break;
+    if (error && error.response) {
+      switch (error.response.status) {
+        case 400:
+          error.message = "请求错误(400)";
+          break;
+        case 401:
+          error.message = "未授权，请重新登录(401)";
+          break;
+        case 403:
+          error.message = "拒绝访问(403)";
+          break;
+        case 404:
+          error.message = "请求出错(404)";
+          break;
+        case 408:
+          error.message = "请求超时(408)";
+          break;
+        case 500:
+          error.message = "服务器错误(500)";
+          break;
+        case 501:
+          error.message = "服务未实现(501)";
+          break;
+        case 502:
+          error.message = "网络错误(502)";
+          break;
+        case 503:
+          error.message = "服务不可用(503)";
+          break;
+        case 504:
+          error.message = "网络超时(504)";
+          break;
+        case 505:
+          error.message = "HTTP版本不受支持(505)";
+          break;
+        default:
+          error.message = `连接出错(${error.response.status})!`;
+      }
+    } else {
+      error.message = "连接服务器失败!";
     }
+    Message.error({ message: error.message });
     console.log("响应异常信息：" + error);
 
     // 获取错误信息
